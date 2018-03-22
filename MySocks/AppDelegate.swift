c0afdae20bd1449cb985a9ad6bc110cb23a00b71
Yu Luo <luoyujx@gmail.com>
2018-03-22 20:30:09 +0900

    Initial Commit

---
+++ MySocks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MySocks
+//
+//  Created by yuluo on 2018/03/22.
+//  Copyright © 2018 yuluo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MySocks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MySocks
+//
+//  Created by yuluo on 2018/03/22.
+//  Copyright © 2018 yuluo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
